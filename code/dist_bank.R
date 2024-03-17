f9bbb608ec02dd4c393d232d832816f397833a34
Lindsey Bell <lindseybell@arizona.edu>
2024-03-17 16:45:43 -0700

    initial commit- script to calc dist from bank for each cell

---
+++ code/dist_bank.R
@@ -0,0 +1,4 @@
+#calculating distance from bank paramter 
+#calculated for the position of each cell, so is constant for each year
+#shapefile includes two lines running along the north most and south most edges of the river
+
