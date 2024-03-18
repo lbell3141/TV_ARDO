d5dcf24adc9771edb8d5f5536b45799443db517a
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-03-18 13:37:39 -0700

    save rast

--- code/dist_bank.R
+++ code/dist_bank.R
@@ -1,4 +1,4 @@
-#calculating distance from bank paramter 
+#calculating distance from bank parameter 
 #calculated for the position of each cell, so is constant for each year
 #shapefile includes two lines running along the north most and south most edges of the river
 
@@ -7,11 +7,11 @@ library(sf)
 
 pathtoBankShapefile <- "./data/TV_Banks/TV_Banks.shp"
 pathtoReferenceRast <- "./data/LiDAR/chm.tif"
-pathtoRastOutput <- "./data/TV_Banks"
+pathtoRastOutput <- "./data/TV_Banks/bank_dist.tif"
 
 centerline <- vect(pathtoBankShapefile)
 ref_rast <- rast(pathtoReferenceRast)
 
 dist_rast <- distance(ref_rast, centerline)
 
-
+writeRaster(dist_rast,pathtoRastOutput,overwrite=TRUE)
