22a9c52cf0d47106c4d2cf1b04c07f728287ace5
Lindsey Bell <lindseybell@arizona.edu>
2024-03-17 19:48:27 -0700

    train/test prepro added

--- code/data_prepro.R
+++ code/data_prepro.R
@@ -8,6 +8,10 @@ pathtoDistRast <- "./data/TV_Banks/TV_Banks.tif"
 pathtoNDVIstack <- "./data/NDVI_stack.tif"
 pathtoNDVIstack <- "./data/PlanetScopeScenes/Jun23_Oct23/PSScene/20230602_171215_24_24c3_3B_AnalyticMS_SR_clip.tif"
 
+pathtoARDOpts <- "./data/object_points/ARDO_pts/ARDO_pts.shp"
+pathtoOtherpts <- "./data/object_points/Other_pts/otherpts.shp"
+pathtoCSVoutput <- "./data/model/train_test_data.csv"
+
 height_rast <- rast(pathtoCHMrast)
 dist_rast <- rast(pathtoDistRast)
 NDVI_rast_stack <- rast(pathtoNDVIstack)
@@ -24,3 +28,26 @@ NDVI_df <- as.data.frame(NDVI_rast_stack, xy = T)
 #merge data frames by xy positions (need to have same CRS!)
 merged_df <- merge(heights_df, dist_df, by = c("x", "y"))
 merged_df <- merge(merged_df, NDVI_df, by = c("x", "y"))
+
+#===============================================================================
+#extract training/testing points and build dataframe
+
+ARDOpts <- vect(pathtoARDOpts)
+Otherpts <- vect(pathtoOtherpts)
+
+full_stack <- rast(list(height_rast, dist_rast, NDVI_rast_stack))
+
+ARDO_stack_vals <- extract(full_stack, ARDOpts)
+ARDO_stack_vals$Group <- "ARDO"
+ARDO_stack_vals$present <- 1
+Other_stack_vals <- extract(full_stack, Otherpts)
+Other_stack_vals$Group <- "Other"
+Other_stack_vals$present <- 0
+
+combined_df <- rbind(ARDO_stack_vals, Other_stack_vals)
+combined_df$OID <- seq_len(nrow(combined_df))
+
+end_of_NDVI_cols <- ncol(combined_df) - 3
+combined_df <- combined_df[, c(10,1,8,9,2,3:end_of_NDVI_cols)]
+
+write.csv(combined_df, pathtoCSVoutput, row.names = F)
