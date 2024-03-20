c2be968357e45b77418e7f876df8d78c8664bb82
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-03-20 13:59:02 -0700

    used poly for riverbed instead of bank lines

--- code/dist_bank.R
+++ code/dist_bank.R
@@ -3,65 +3,22 @@
 #shapefile includes two lines running along the north most and south most edges of the river
 
 library(terra)
-library(sf)
-library(rgeos)
 library(raster)
 
-pathtoBankShapefile <- "./data/TV_Banks/TV_Banks.shp"
+pathtoBankShapefile <- "./data/TV_Banks/TV_bank_poly.shp"
 pathtoReferenceRast <- "./data/LiDAR/chm.tif"
-pathtoRastOutput <- "./data/TV_Banks/bank_dist.tif"
+pathtoRastOutput <- "./data/TV_Banks/bed_dist.tif"
 
 #load in riverbank lines and raster with reference crs and positions
-banks <- shapefile(pathtoBankShapefile)
-
-centerline <- vect(pathtoBankShapefile)
+banks <- vect(pathtoBankShapefile)
 
 ref_rast <- rast(pathtoReferenceRast)
 
-dist_rast <- distance(ref_rast, centerline)
+bank_raster <- rasterize(banks, ref_rast)
 
-dd <- gDistance(banks, as(ref_rast, "SpatialPoints"), byid = TRUE)
-
-
-
-#calc distance from nearest bank line
-distance_values <- distance(banks)
-
-#place distance values in empty distance raster
-values(dist_rast) <- distance_values
+dist_rast <- distance(bank_raster)
+plot(dist_rast)
 
 #save output distance raster
-writeRaster(dist_rast, filename = file.path(pathtoRastOutputs,"bank_dist"), format="GTiff",overwrite=TRUE)
+writeRaster(dist_rast, pathtoRastOutput,overwrite=TRUE)
 
-
-
-
-
-
-#test
-
-
-library(terra)
-library(sf)
-library(rgeos)
-library(rgdal)
-
-pathtoBankShapefile <- "./data/TV_Banks/TV_Banks.shp"
-pathtoReferenceRast <- "./data/LiDAR/chm.tif"
-pathtoRastOutput <- "./data/TV_Banks"
-
-#load in riverbank lines and raster with reference crs and positions
-banks <- readOGR(pathtoBankShapefile)
-
-ref_rast <- rast(pathtoReferenceRast)
-
-#create empty rast to store distance values in
-dist_rast <- rast(ref_rast)
-
-dd <- gDistance(banks, as(ref_rast, "SpatialPoints"), byid = TRUE)
-
-
-spatial_lines <- SpatialLines(list(Lines(list(Line(coordinates(shapefile))), ID = "1")))
-=======
-writeRaster(dist_rast,pathtoRastOutput,overwrite=TRUE)
->>>>>>> 4bad8488a8c65f08a5505117ba9fb0c599b2dda9
