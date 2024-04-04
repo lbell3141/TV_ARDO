7f489f43325431c30ce6fd8e31b281f20204b8e7
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-04-03 19:31:16 -0700

    renamed and adjusted NDVI filter

--- code/prep_fulldata.R
+++ code/prep_fulldata.R
@@ -6,17 +6,19 @@
 #model: present ~ 0.5 height, 0.5 distance from channel center, 0.5 NDVI April 12 2022
 
 #preproc===============================================
+library(terra)
 
 pathtoCHMrast <- "./data/LiDAR/chm_filtered.tif"
 pathtoDistRast <- "./data/TV_Banks/bed_dist.tif"
 pathtoNDVI <- "./data/20_21_NDVI_stack.tif"
 
 pathtoTTPARast <- "./data/model/TT_PA.tif"
-pathtoCSVoutput <- "./data/model/rast_model/TT_data.csv"
+pathtoCSVoutput <- "./data/model/rast_model/TT_data_rastmodel.csv"
 
 height_rast <- rast(pathtoCHMrast)
 dist_rast <- rast(pathtoDistRast)
 NDVI_rast <- rast(pathtoNDVI)
+  NDVI_rast[NDVI_rast < 0.18] <- NA
 TTPA_rast <- rast(pathtoTTPARast)
 #change raster resolution to all be the same
 height_rast <- resample(height_rast, NDVI_rast)
@@ -28,7 +30,7 @@ heights_df <- as.data.frame(height_rast, xy = T)
 dist_df <- as.data.frame(dist_rast, xy = T)
 NDVI_df <- as.data.frame(NDVI_rast, xy = T)
 TTPA_df <- as.data.frame(TTPA_rast, xy = T, na.rm = F)
-TTPA_df[is.na(TTPA_df)] <- 0
+  TTPA_df[is.na(TTPA_df)] <- 0
 
 merged_df <- merge(heights_df, dist_df, by = c("x", "y"))
 merged_df <- merge(merged_df, NDVI_df, by = c("x", "y"))
@@ -49,6 +51,6 @@ colnames(merged_df)[6] <- "dist_bank"
 
 #add stipulations for height and ground NDVI
 #height already filtered in OHM script
-merged_df <- merged_df[merged_df$NDVI >= 0.18, ]
+#merged_df <- merged_df[merged_df$NDVI >= 0.18, ]
 
 write.csv(merged_df, pathtoCSVoutput, row.names = F)
