1cd0770c0998467073400c65f757dc1e82401114
Lindsey Bell <lindseybell@snrenet.arizona.edu>
2024-05-01 15:52:13 -0700

    compared PSS data but patch size impacts diff too much. trying post monsoon half meter product next

--- code/removal_plot.R
+++ code/removal_plot.R
@@ -6,14 +6,82 @@
 
 library(sf)
 library(terra)
+library(ggplot2)
 
-pathtoKMLfile <-  "./Untitled Polygon.kml"
+pathtoshpfile <-  "./indiv_poly/indiv_poly-polygon.shp"
 pathto2021rasts <- "./data/20_21_NDVI_stack.tif"
 pathto2023rasts <- "./data/22_23_NDVI_stack.tif"
+pathtoHM21rast <- 
+pathtoHM23rast <- 
 
 
-ARDO_kml <- st_read(pathtoKMLfile)
-ARDO_kml <- ARDO_kml[1]
-
+#load data
+ARDO_sf <- vect(pathtoshpfile)
 stack_21 <- rast(pathto2021rasts)
 stack_23 <- rast(pathto2023rasts)
+
+#reproject sf to raster crs
+ARDO_sfcrs <- project(ARDO_sf, stack_21)
+
+#find average value of pixels contained within each ARDO polygon
+#make sure GEP file is not multigeometry
+avg21_poly_vals <- extract(stack_21, ARDO_sfcrs, fun = mean, na.rm = TRUE)
+avg23_poly_vals <- extract(stack_23, ARDO_sfcrs, fun = mean, na.rm = TRUE)
+
+#compare values to determine removal
+
+NDVI_21 <- avg21_poly_vals[,43]
+NDVI_21 <- as.data.frame(NDVI_21)
+NDVI_23 <- avg23_poly_vals[,149]
+NDVI_23 <- as.data.frame(NDVI_23)
+comp_df <- cbind(NDVI_21, NDVI_23)
+comp_df$difference <- comp_df$NDVI_23 - comp_df$NDVI_21
+
+plot_comp <- ggplot(data = comp_df, mapping = aes(x = seq_along(NDVI_21))) +
+  geom_point(aes(y = NDVI_21, color = "NDVI_21")) +
+  geom_point(aes(y = NDVI_23, color = "NDVI_23")) +
+  labs(x = "Polygon #", y = "NDVI", color = "Variable") +
+  scale_color_manual(values = c("NDVI_21" = "blue", "NDVI_23" = "red")) +
+  theme_minimal()
+plot_comp
+
+plot_dif <- ggplot(data = comp_df, mapping = aes(x = seq_along(NDVI_21))) +
+  geom_point(aes(y = difference)) +
+  labs(x = "Polygon #", y = "Difference in NDVI (2023 - 2021)") +
+  theme_minimal()
+plot_dif
+
+comp_df$removed <- comp_df$difference < 0.1
+
+#===================with 0.5m data==============================================
+#compares Nov 2020 to Sept 2023
+
+#load data
+hm21 <- rast(pathtoHM21rast)
+hm23 <- rast(pathtoHM23rast)
+
+hm_avg21_poly_vals <- extract(hm21, ARDO_sfcrs, fun = mean, na.rm = TRUE)
+hm_avg23_poly_vals <- extract(hm23, ARDO_sfcrs, fun = mean, na.rm = TRUE)
+
+
+hm_NDVI_21 <- hm_avg21_poly_vals[,43]
+hm_NDVI_21 <- as.data.frame(hm_NDVI_21)
+hm_NDVI_23 <- hm_avg23_poly_vals[,149]
+hm_NDVI_23 <- as.data.frame(hm_NDVI_23)
+hm_comp_df <- cbind(hm_NDVI_21, hm_NDVI_23)
+hm_comp_df$difference <- hm_omp_df$hm_NDVI_23 - hm_comp_df$hm_NDVI_21
+
+hm_plot_comp <- ggplot(data = hm_comp_df, mapping = aes(x = seq_along(hm_NDVI_21))) +
+  geom_point(aes(y = hm_NDVI_21, color = "hm_NDVI_21")) +
+  geom_point(aes(y = hm_NDVI_23, color = "hm_NDVI_23")) +
+  labs(x = "Polygon #", y = "NDVI", color = "Variable") +
+  scale_color_manual(values = c("NDVI_21" = "blue", "NDVI_23" = "red")) +
+  theme_minimal()
+hm_plot_comp
+
+hm_plot_dif <- ggplot(data = hm_comp_df, mapping = aes(x = seq_along(hm_NDVI_21))) +
+  geom_point(aes(y = difference)) +
+  labs(x = "Polygon #", y = "Difference in NDVI (2023 - 2021)") +
+  theme_minimal()
+hm_plot_dif
+
